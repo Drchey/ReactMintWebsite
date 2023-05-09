c2939caecac50e7a67682d180bb99e5fc0107816
drchey <derichey9@gmail.com>
2023-05-09 12:32:01 +0100

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Navbar } from './component'
+import styles from './style'
+
 function App() {
-  return <div className="bg-forest w-full overflow-hidden"></div>
+  return (
+    <div className="bg-dimWhite w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
