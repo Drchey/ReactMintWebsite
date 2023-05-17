649cba85bc70e828155cd18d1febaa60486fe9cd
drchey <derichey9@gmail.com>
2023-05-17 11:46:22 +0100

    Add Props to Contact Us Props

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,3 +1,4 @@
+import { hero } from '../assets'
 import { Background, Footer, Navbar } from '../component'
 import ContactForm from '../component/ContactForm'
 import styles from '../style'
@@ -11,7 +12,15 @@ const Contact = () => (
     </div>
     <div className={`bg-dimWhite  ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        <Background />
+        <Background
+          img={hero}
+          classImg={`w-full h-[60vh] object-fit`}
+          title="Contact Us"
+          context="Kindly get in touch with us for any business proposals, enquires, comments or suggestions."
+          btn="Contact Us"
+          classContext={`absolute top-0 left-0 z-10 w-full h-full flex justify-center items-center bg-teal-800 opacity-80`}
+          classBtn={`bg-dimWhite mt-20 p-4 w- z-19[150px] h-[70px] px-9 text-forest cursor-none  border-none  font-bold`}
+        />
       </div>
     </div>
 
