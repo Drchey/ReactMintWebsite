83f961913c698e90ad4aef87bb7c1ce192fd9a0c
drchey <derichey9@gmail.com>
2023-05-22 04:44:53 +0100

    [fix] Adjust Coolor  Gradient to Fix Link to Services Nav

--- src/component/ProductHeader.js
+++ src/component/ProductHeader.js
@@ -3,11 +3,10 @@ import styles from '../style'
 import { Link } from 'react-router-dom'
 
 const ProductHeader = ({ title }) => (
-  <div class={`flex mt-5`}>
-    <div class="flex-none w-44">
+  <div className={`flex mt-5`}>
+    <div className="flex-none w-44">
       <h2 className={styles.heading2}> VAC </h2>
-      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
+
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
     </div>
     <div className={`flex-initial w-84 ...`}>
